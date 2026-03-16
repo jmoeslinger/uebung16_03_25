9d3a1c2e0f86d8674196415c5f3b1a13e8367f19
Jakub Möslinger <moslingerj@gmail.com>
2026-03-16 20:41:41 +0100

    adding 4th assignment

---
+++ UebungLampe/Lampe.cs
@@ -0,0 +1,31 @@
+﻿namespace UebungLampe
+{
+    public class Lampe
+    {
+        public string Farbe { get; set; }
+        public bool istAn { get; set; }
+
+
+        public void Einschalten()
+        {
+            istAn = true;
+        }
+
+        public void Ausschalten()
+        {
+            istAn = false;
+        }
+
+        public void StatusAnzeigen()
+        {
+            if (istAn)
+            {
+                Console.WriteLine($"Die Lampe ist an und hat die Farbe {Farbe}.");
+            }
+            else
+            {
+                Console.WriteLine("Die Lampe ist aus und hat die Farbe {Farbe}.");
+            }
+        }
+    }
+}
---
+++ UebungLampe/Program.cs
@@ -0,0 +1,8 @@
+﻿using UebungLampe;
+
+Lampe lampe1 = new Lampe();
+
+lampe1.Farbe = "Rot";
+lampe1.Einschalten();
+
+lampe1.StatusAnzeigen();
