acdb1b5878fbe06464565d2c861c8e4d8c58eea6
Jakub Möslinger <moslingerj@gmail.com>
2026-03-16 20:30:02 +0100

    adding 3rd assignment

---
+++ UebungSchueler/Program.cs
@@ -0,0 +1,9 @@
+﻿using UebungSchueler;
+
+Student studentmax = new Student();
+
+studentmax.Surname = "Mustermann";
+studentmax.Firstname = "Max";
+studentmax.Mainclass = "2BHK";
+
+studentmax.SayHello();
---
+++ UebungSchueler/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace UebungSchueler;
+
+public class Student
+{
+    public string Firstname { get; set; }
+    public string Surname { get; set; }
+    public string Mainclass { get; set; }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hallo, mein Name ist {Firstname} {Surname} und ich Schüler der {Mainclass}.");
+    }
+
+}
