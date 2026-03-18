0c74c79cfd5c500b30623fb46f6d5cbd16196849
Jakub Möslinger <moslingerj@gmail.com>
2026-03-18 11:08:07 +0100

    adding 9th assignment

---
+++ UebungTempreatur/Program.cs
@@ -0,0 +1,9 @@
+﻿using UebungTempreatur;
+
+Temperatur temp = new Temperatur();
+
+temp.Grad = 20;
+
+temp.Anzeigen();
+temp.Erhoehen();
+temp.Senken();
---
+++ UebungTempreatur/Temperatur.cs
@@ -0,0 +1,26 @@
+﻿
+namespace UebungTempreatur
+{
+    public class Temperatur
+    {
+
+        public int Grad { get; set; }
+
+        public void Anzeigen()
+        {
+            Console.WriteLine($"Die aktuelle Temperatur beträgt {Grad} Grad.");
+        }
+        public void Erhoehen()
+        {
+            Grad += 1;
+            Console.WriteLine($"Die Temperatur wurde um 1°C erhöht und liegt nun bei {Grad}°C. ");
+        }
+
+        public void Senken()
+        {
+            Grad -= 1;
+            Console.WriteLine($"Die Temperatur wurde um 1°C gesenkt und liegt nun bei {Grad}°C. ");
+        }
+
+    }
+}
