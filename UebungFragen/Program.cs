60895410ca711d868d9acb1e17dfb8b887c0564f
Jakub Möslinger <moslingerj@gmail.com>
2026-03-18 10:32:09 +0100

    adding 6th assignment

---
+++ UebungFragen/Program.cs
@@ -0,0 +1,7 @@
+﻿// 1. Frage: Was ist der Unterschied zwischen Klasse und Objekt?
+// 2. Frage: Warum verwendet man Namespaces?
+// 3. Frage: Was ist der Unterschied zwischen Eigenschaft und Variable?
+
+// 1. Antwort: Eine Klasse ist eine Vorlage für die Erstellung von Objekten.
+// 2. Antwort: Namespaces sind für Organisation/Übersicht da. Sie sorgen auch für keine Namenskonflikte.
+// 3. Antwort: In einer Variable werden Daten gespeichert, eine Eigenschaft ist eine get oder set Methode.
