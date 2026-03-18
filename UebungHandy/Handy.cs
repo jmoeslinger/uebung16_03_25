bf49cb0b62f9964995e5f717b383134536f5b806
Jakub Möslinger <moslingerj@gmail.com>
2026-03-18 10:40:13 +0100

    adding 7th assignment

---
+++ UebungHandy/Handy.cs
@@ -0,0 +1,22 @@
+﻿
+namespace UebungHandy
+{
+    public class Handy
+    {
+
+        public string Marke { get; set; }
+        public string Modell { get; set; }
+        public int AkkuProzent { get; set; }
+
+        public void Anzeigen()
+        {     Console.WriteLine($"Handy: {Marke}: {Modell}");
+            Console.WriteLine($"Akku: {AkkuProzent}%");
+        }
+        public void Aufladen() 
+        { Console.WriteLine($"Akku wird aufgeladen...");
+            AkkuProzent = 100;
+            Console.WriteLine($"Akku: {AkkuProzent}%");
+        }
+
+    }
+}
---
+++ UebungHandy/Program.cs
@@ -0,0 +1,10 @@
+﻿using UebungHandy;
+
+Handy samsungHandy  = new Handy();
+
+samsungHandy.Marke = "Samsung";
+samsungHandy.Modell = "Galaxy S23";
+samsungHandy.AkkuProzent = 45;
+
+samsungHandy .Anzeigen();
+samsungHandy .Aufladen();
