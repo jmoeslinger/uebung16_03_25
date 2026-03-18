8e5cdb1a22b6225e37dfce479a7a71d94059e741
Jakub Möslinger <moslingerj@gmail.com>
2026-03-18 11:14:46 +0100

    adding the last/10th assignment

---
+++ letzteUebungFernseher/Fernseher.cs
@@ -0,0 +1,28 @@
+﻿namespace letzteUebungFernseher
+{
+    public class Fernseher
+    {
+        public string Marke { get; set; }
+        public int Kanal { get; set; }
+        public bool IstAn { get; set; }
+
+        public void Einschalten()
+        {
+            IstAn = true;
+            Console.WriteLine($"Fernseher einschalten");
+        }
+
+        public void Ausschalten()
+        {
+            IstAn = false;
+            Console.WriteLine($"Fernseher ausschalten");
+        }
+
+        public void KanalAnzeigen()
+        {
+            Console.WriteLine($"Aktueller Kanal: {Kanal}");
+
+
+        }
+    }
+}
---
+++ letzteUebungFernseher/Program.cs
@@ -0,0 +1,11 @@
+﻿using letzteUebungFernseher;
+
+Fernseher HomeTV = new Fernseher();
+
+HomeTV.Marke = "Samsung";
+HomeTV.Kanal = 5;
+HomeTV.IstAn = false;
+
+HomeTV.Einschalten();
+HomeTV.KanalAnzeigen();
+HomeTV.Ausschalten();
