6ec7e0c0c236bd0cac44d2cf1d11a4b932a16957
Jakub Möslinger <moslingerj@gmail.com>
2026-03-18 09:58:59 +0100

    adding 5th assignment

---
+++ UebungHund/Hund.cs
@@ -0,0 +1,24 @@
+﻿namespace UebungHund
+{
+    public class Hund
+    {
+        public string Name { get; set; }
+        public string Rasse { get; set; }
+        public int Alter { get; set; }
+
+        public void Bellen()
+        {
+            Console.WriteLine($"Der Hund {Name} bellt.");
+        }
+        public void Info()
+        {
+            Console.WriteLine(
+                $"Name: {Name},\n" +
+                $"Rasse: {Rasse},\n" +
+                $"Alter: {Alter} Jahre"
+                );
+
+
+        }
+    }
+}
---
+++ UebungHund/Program.cs
@@ -0,0 +1,9 @@
+﻿using UebungHund;
+
+Hund hund1 = new Hund ();
+hund1.Name = "Bello";
+hund1.Rasse = "Labrador";
+hund1.Alter = 4;
+
+hund1.Bellen();
+hund1.Info();
