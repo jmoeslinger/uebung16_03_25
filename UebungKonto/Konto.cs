141bccfe24d1e11bfbdd0084b4e41d3a40c8faa2
Jakub Möslinger <moslingerj@gmail.com>
2026-03-18 10:58:56 +0100

    adding 8 assignment

---
+++ UebungKonto/Konto.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace UebungKonto
+{
+    public class Konto
+    {
+        public string Inhaber { get; set; }
+        public int Kontostand { get; set; }
+
+        public void KontostandAnzeigen()
+        {
+            Console.WriteLine($"Kontoinhaber: {Inhaber} \nKontostand: {Kontostand}");
+        }
+        public void Einzahlen()
+        {
+            Kontostand += 50;
+            Console.WriteLine($"Einzahlung erfolgt \nKontostand: {Kontostand}");
+
+        }
+    }
+}
---
+++ UebungKonto/Program.cs
@@ -0,0 +1,9 @@
+﻿using UebungKonto;
+
+Konto annaskonto = new Konto();
+
+annaskonto.Inhaber = "Anna";
+annaskonto.Kontostand = 200;
+
+annaskonto.KontostandAnzeigen();
+annaskonto.Einzahlen();
